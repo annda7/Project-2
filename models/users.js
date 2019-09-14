d744d47fa2704b6c0c500dea95dbc7f62e9619f7
Angelo DePaolo <adepaolo@gmail.com>
2019-09-14 11:38:34 -0700

    updated files in models folder and mysql schema

--- models/users.js
+++ models/users.js
@@ -1,9 +1,47 @@
-module.exports = function(sequelize, DataTypes) {
-  var Users = sequelize.define("Users", {
-    first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
-    username: DataTypes.STRING,
-    password: DataTypes.STRING
+// Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
+var bcrypt = require("bcryptjs");
+// Creating our User model
+module.exports = function (sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    // The username cannot be null, and must be unique before creation
+    //Passport field ids:  username, first_name, last_name, password
+
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   });
-  return Users;
+  // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+  User.prototype.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  // Hooks are automatic methods that run during various phases of the User Model lifecycle
+  // In this case, before a User is created, we will automatically hash their password
+  User.addHook("beforeCreate", function (user) {
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+  });
+  User.associate = function (models) {
+    User.hasMany(models.toDo, {
+      onDelete: 'cascade',
+      foreignKey: 'createdBy',
+      sourceKey: 'id'
+    });
+  };
+  return User;
 };
